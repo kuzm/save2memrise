52cee6405143bc90ac07ab1d7508a9214f1e7ecf
Mikhail Kuzmiankou <m@kuzmiankou.com>
2019-04-07 20:41:56 +0200

    Browser extension v0.0.13

--- src/BrowserExts/ChromeExt/config.js
+++ src/BrowserExts/ChromeExt/config.js
@@ -1,10 +1,10 @@
 if (typeof module !== 'undefined' 
         && typeof module.exports !== 'undefined') {
     // Constants for frame.js
-    module.exports.save2memriseApiUrl = "http://localhost:5001/v1/";
+    module.exports.save2memriseApiUrl = "https://api2.save2memrise.com/v1/";
     module.exports.closeWindowTimeout = 10000;
-    module.exports.extensionId = 'bhjnibbfgdjnooiaplgfhgfibclgmpno'; // local
+    module.exports.extensionId = 'jedpoleopoehklpioonelookacalmcfk'; // web store
 } else {
     // Constants for popup.js
-    window.frameUrl = "frame.html";
+    window.frameUrl = "https://chromeext2.save2memrise.com/frame.html";
 }
